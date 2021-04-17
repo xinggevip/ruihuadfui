073a5b7b235079a1526a08558f4d17f9be890d51
xinggevip <1511844263@qq.com>
2021-04-17 19:03:05 +0800

    打分

--- src/components/Dafen.vue
+++ src/components/Dafen.vue
@@ -7,21 +7,32 @@
       @click-left="onClickLeft"
     />
     
-    <div class="father">
-      <h1>打分</h1>
+    <div class="father" v-if="scoreItems.length > 0">
+      <!-- <h4>当前环节：{{stepName}}</h4> -->
+      <!-- <h3 style="color:#ccc">此环节满分为：{{manfen}}</h3> -->
+      <p><b>您给出的分数为：</b><span style="font-size:22px">{{currentScore}}</span><span style="color:#ccc;font-size:14px">&nbsp;&nbsp;&nbsp;(满分为{{manfen}})</span></p>
         <div class="numCount-father">
           <div class="numCount" v-for="(item,index) in scoreItems" :key="index">
-          <span class="numCount-title">{{item.itemName}}</span>
+          <span class="numCount-title">{{index + 1}}. {{item.itemName}}</span>
           <span class="numcount-tip">(最高{{item.maxScore}}分)</span>
-          <van-stepper :v-model="item.value" :step="Math.round(item.maxScore / 10)" input-width="60px" button-size="32px"  min="0" :max="item.maxScore" />
+          <van-stepper @change="onChange" :default-value="0" v-model="scoreItems[index].value" :step="Math.round(item.maxScore / 10)" input-width="60px" button-size="32px"  min="0" :max="item.maxScore" />
         </div>
       </div>
+
+      <van-button @click="submit" plain  round type="info" style="width:100%;margin-top:30px;">提交</van-button>
       
       
+    </div>
+    <div class="father" v-else>
+      <h3>加载中...</h3>
     </div>
 
 
   </div>
+
+
+
+  
 </template>
 
 
@@ -42,6 +53,9 @@ export default {
       player:null,
       scoreItems:[],
       value:0,
+      manfen:0,
+      currentScore:0,
+      stepName:""
     }
   },
   created(){
@@ -52,8 +66,9 @@ export default {
   methods:{
 
     fetchData(){
+      this.getStep()
       this.getPlayerById();
-      this.getCurrentStepItemList();
+      this.getCurrentStepScoreItemList();
     },
     getPlayerById(){
       let playerid = this.$route.params.playerid;
@@ -68,14 +83,33 @@ export default {
 
       })
     },
-    getCurrentStepItemList(){
+    getCurrentStepScoreItemList(){
       let actid = this.$route.params.actid;
       let params = {
         actid:actid
       };
       this.$postQs("/step/currentScoreList",params).then(response=>{
         this.scoreItems = response.data.data;
+        this.manfen = 0;
+        this.scoreItems.forEach((item,index,arr)=>{
+          this.manfen = this.manfen + item.maxScore;
+        })
         console.log(this.scoreItems);
+      }).catch(err=>{
+
+      }).finally(()=>{
+
+      })
+    },
+    getStep(){
+      let params = {
+        id:this.$route.params.actid
+      }
+      this.$postQs("/step/currentStep",params).then(response=>{
+        console.log(response);
+        if(response.data.success){
+            this.stepName = response.data.data.title;
+        }
       }).catch(err=>{
 
       }).finally(()=>{
@@ -86,6 +120,38 @@ export default {
     onClickLeft(){
       window.history.back(-1);
     },
+    onChange(value) {
+      this.currentScore = 0;
+      this.scoreItems.forEach((item,index,arr)=>{
+        this.currentScore = this.currentScore + item.value; 
+      })
+    },
+    submit(){
+      // alert("提交")
+      // 拷贝一份数组
+      let params = this.scoreItems.map((item,index,arr)=>{
+        let res = {
+          scoreitemId:item.id,
+          judgeId:this.$route.params.judgeid,
+          playerId:this.$route.params.playerid,
+          numValue:item.value
+        }
+        return res;
+      });
+
+      // console.log(params);
+
+      this.$post("/scorevalue/dafen",params).then(response=>{
+        console.log(response);
+      }).catch(err=>{
+
+      }).finally(()=>{
+
+      })
+
+   
+
+    }
 
 
 
@@ -114,6 +180,10 @@ export default {
     right: 0px;
     top: 50%;
     transform: translateY(-50%);
+  }
+  .white-space {
+             white-space:pre
+
   }
 }
 
--- vue.config.js
+++ vue.config.js
@@ -2,18 +2,19 @@
 module.exports = {
 
   devServer: {
+    disableHostCheck: true,
     port: 8083,
-    proxy: {
-      '/api': {
-        // target: 'http://shitz.cn:8181',
-        target: 'http://47.102.220.144:8181',
+    // proxy: {
+    //   '/api': {
+    //     // target: 'http://shitz.cn:8181',
+    //     target: 'http://47.102.220.144:8181',
 
-        ws: true,
-        changeOrigin: true,
-        // pathRewrite: {
-        //   '^/api': ''//需要重写的
-        // }
-      }
-    }
+    //     ws: true,
+    //     changeOrigin: true,
+    //     // pathRewrite: {
+    //     //   '^/api': ''//需要重写的
+    //     // }
+    //   }
+    // }
   }
 }
